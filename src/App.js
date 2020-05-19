9ac5704840d8ab7b535f6ec4c61466ba21365a5b
Aiste Neciunaite <a.neciunaite@gmail.com>
2020-05-19 16:17:40 +0300

    My first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="rectangle4"></div>
+        <div className="rectangle1">
+          <h1 className="name">SOPHIE ALPERT</h1>
+          <h3 className="rectangle3">PROGRAMMER</h3>
+        </div>
+      </div>
+      <div className="App-links">
+        <h2 className="underline">LINKS</h2>
+      </div>
+      <div className="App-about">
+        <h2 className="underline">ABOUT</h2>
+      </div>
+      <div className="App-education">
+        <h2 className="underline">EDUCATION</h2>
+      </div>
+      <div className="App-skills">
+        <h2 className="underline">PERSONAL SKILLS</h2>
+      </div>
+      <div className="App-technical">
+        <h2 className="underline">TECHNICAL SKILLS</h2>
+      </div>
+      <div className="App-experience">
+        <h2 className="xp-title underline">WORK EXPERIENCE</h2>
+        <div className="xp-position"></div>
+        <div className="xp-position"></div>
+        <div className="xp-position"></div>
+      </div>
+      <div className="App-contact">
+        <div className="underline"></div>
+      </div>
+      <div>
+        <div className="rectangle3">
+          <h3>ADDRESS</h3>
+        </div>
+      </div>
+      <div>
+        <div className="rectangle3">
+          <h3>CONTACT</h3>
+        </div>
+      </div>
+      <div>
+        <div className="rectangle3">
+          <h3>SOCIAL</h3>
+        </div>
+      </div>
     </div>
   );
 }
